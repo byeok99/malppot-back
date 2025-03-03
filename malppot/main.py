7a8093432049473bc87139b5447c5b408008a3c2
byeok99 <tkdqur3203@gmail.com>
2025-03-03 23:15:36 +0900

    feat: jwt 미들웨어 추가

--- malppot/main.py
+++ malppot/main.py
@@ -2,15 +2,19 @@ import sys
 import uvicorn
 from pathlib import Path
 from malppot import create_app
-from malppot.di.config import ConfigContainer
+from fastapi.staticfiles import StaticFiles
+from malppot.utils.middleware import JWTMiddleware
 
 PROJECT_ROOT = Path(__file__).parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
 app = create_app()
-config = ConfigContainer()
+
+app.add_middleware(JWTMiddleware)
+# 임시
+app.mount("/static", StaticFiles(directory=f"{PROJECT_ROOT}/malppot/static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
-# uvicorn malppot.app:app --host 0.0.0.0 --port 8000 --reload
+# uvicorn malppot.main:app --host 0.0.0.0 --port 8000 --reload
---
+++ malppot/utils/middleware.py
@@ -0,0 +1,31 @@
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+import jwt
+from starlette.responses import JSONResponse
+from malppot.di import DI
+
+
+class JWTMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app):
+        super().__init__(app)
+        self.secret_key = DI.config.jwt.secret_key
+        self.algorithm = DI.config.jwt.algorithm
+
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path.startswith("/auth"):
+            return await call_next(request)
+        token = request.headers.get("Authorization")
+        if token and token.startswith("Bearer "):
+            token = token.split(" ")[1]
+            try:
+                payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
+                request.state.user = payload["sub"]
+            except jwt.ExpiredSignatureError:
+                return JSONResponse({"detail": "Token expired"}, status_code=401)
+            except jwt.InvalidTokenError:
+                return JSONResponse({"detail": "Invalid token"}, status_code=401)
+
+        else:
+            return JSONResponse({"detail": "Authorization hearder missing"}, status_code=401)
+
+        return await call_next(request)
