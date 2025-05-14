b53a70d52051b43c77a4394072a1b2853ce4b31a
kk7146 <kk21322132@icloud.com>
2025-05-14 18:32:44 +0900

    fix:db_session

--- malppot/domain/heygen/service.py
+++ malppot/domain/heygen/service.py
@@ -54,6 +54,7 @@ class HeyGenService:
     async def generate_video(self, script: str, user_idx:int) -> str:
         try:
             one_week_ago = datetime.utcnow() - timedelta(days=7)
+            session = self.db.get_session()
 
             # 1. 최근 7일 이내 동일 스크립트 조회
             result = session.execute(
