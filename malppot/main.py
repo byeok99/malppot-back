020ecb6ff1d151e95df9f84a677bfaccb1b3f4b3
byeok99 <tkdqur3203@gmail.com>
2025-06-30 13:45:13 +0900

    Update main.py

--- malppot/main.py
+++ malppot/main.py
@@ -32,7 +32,11 @@ wiring.wire(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],
+    allow_origins=[
+        "http://localhost:5174",
+        "https://www.malppot.com",
+        "https://malppot.com"]
+    ,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
